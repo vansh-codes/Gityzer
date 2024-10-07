ccbb5f465ba50b854f5a7ef104a7d833a1d7478c
Tamanna <tamannamalik11a25@gmail.com>
2024-10-07 22:13:15 +0530

    Added Home Page Conataining basic Setup of the Landing Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
-export default function Home() {
+import Home from "../../components/Home";
+
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <Home />
+/*     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-6xl text-center">GITYZER</h1>
         <p className="text-center -ml-4 ">Take UI inspiration from <a className="text-center text-blue-500 cursor-pointer hover:underline" href="https://socialify.git.ci/">GitHub Socialify</a></p>
       </main>
-    </div>
+    </div> */
   );
 }
