a0cd3e17024d1ae2b90cf694eafe3215694f31da
Murtaza Sadriwala <msadriwala9@gmail.com>
2024-10-10 00:28:59 +0530

    Issues Resolved

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,7 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { generateUserTagline } from "../api/ai/route";
+import Home from "../../components/Home";
 
-function UserPage({ params }) {
-  const { username } = params;
-  const router = useRouter();
-  const [tagline, setTagline] = useState("");
-
-  useEffect(() => {
-    const fetchTagline = async () => {
-      const response = await fetch(`/api/ai?username=${username}`);
-      const data = await response.json();
-      if (data.exists) {
-        setTagline(data.tagline);
-      } else {
-        setTagline("No tagline available");
-      }
-    };
-    fetchTagline();
-  }, [username]);
-
+export default function Page() {
   return (
-    <div>
-      <h1>{tagline}</h1>
-    </div>
+    <Home />
   );
 }
-
-export default UserPage;
