65f8c91b6d3d867d7f3bb6d010e16050cb55aaef
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-11-01 22:09:57 +0530

    Remove Hacktoberfest consent from ISSUE TEMPLATES

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['avatars.githubusercontent.com'],
-      },
+        domains: ["avatars.githubusercontent.com"],
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*", // Or specify your allowed origin
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
