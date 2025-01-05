4aefa196e4a4af1ce4338d1ebfddc9cb78b32e8e
Abhash Chakraborty <abhashchak14@gmail.com>
2025-01-06 01:21:54 +0530

    ⬆️ Made differnt modules for each api
    👨‍💻Changes:
    - Added generate_tagline api
    - Added user api for github_profile
    - Added generate_tagline api

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,21 +1,21 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-export const POST = async (request: Request) => {
+export const POST = async (request: Request): Promise<NextResponse> => {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
-  try{
+  try {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const data = await request.json()
-    const prompt = data.prompt
+    const data = await request.json();
+    const prompt = data.prompt;
 
     const result = await model.generateContent(prompt);
-    const response = result.response
+    const response = result.response;
     const tagline = response.text();
 
-    return NextResponse.json({ response: tagline })
+    return NextResponse.json({ response: tagline });
   } catch (error) {
-    return NextResponse.json({ error: "Failed to generate content." })
+    return NextResponse.json({ error: "Failed to generate content." });
   }
-}
+};
